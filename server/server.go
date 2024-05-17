995689ac7f8745ccc2b88674281da0f037154a2e
AliiAhmadi <aliahmadi.github@gmail.com>
2024-05-17 10:55:30 +0330

    main: add cors middleware to fiber

--- server/server.go
+++ server/server.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type App struct {
-	localDSP  string
-	remoteDSP string
+	LocalDSP  string
+	RemoteDSP string
+	FilePath  string
 }
 
-type SendRequest struct{}
+type SendRequest struct {
+	Remote string `json:"remote"`
+	Local  string `json:"local"`
+}
 
 type SendResponse struct{}
 
-type ReceiveRequest struct{}
+type ReceiveRequest struct {
+	Remote string `json:"remote"`
+	Local  string `json:"local"`
+}
 
 type ReceiveResponse struct{}
 
+type DSPResponse struct {
+	DSP string `json:"dsp"`
+}
+
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 	ap := App{}
 
 	app.Post("/send", ap.sendHandler) // local -> remote
@@ -36,6 +49,8 @@ func (app *App) sendHandler(c *fiber.Ctx) error { return nil }
 
 func (app *App) receiveHandler(c *fiber.Ctx) error { return nil }
 
-func (app *App) getDSP(c *fiber.Ctx) error { return nil }
+func (app *App) getDSP(c *fiber.Ctx) error {
+	return nil
+}
 
 func (app *App) postDSP(c *fiber.Ctx) error { return nil }
