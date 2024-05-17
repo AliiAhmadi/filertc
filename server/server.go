92e727f450e2f5d54a69ad47c58ee970f3fd9d9b
AliiAhmadi <aliahmadi.github@gmail.com>
2024-05-17 10:41:47 +0330

    develop: developing server routes for call from browser

--- server/server.go
+++ server/server.go
@@ -4,6 +4,38 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type App struct {
+	localDSP  string
+	remoteDSP string
+}
+
+type SendRequest struct{}
+
+type SendResponse struct{}
+
+type ReceiveRequest struct{}
+
+type ReceiveResponse struct{}
+
 func main() {
 	app := fiber.New()
+	ap := App{}
+
+	app.Post("/send", ap.sendHandler) // local -> remote
+
+	app.Post("/receive", ap.receiveHandler) // remote -> local
+
+	app.Get("/dsp", ap.getDSP)
+
+	app.Post("/dsp", ap.postDSP)
+
+	app.Listen(":3000")
 }
+
+func (app *App) sendHandler(c *fiber.Ctx) error { return nil }
+
+func (app *App) receiveHandler(c *fiber.Ctx) error { return nil }
+
+func (app *App) getDSP(c *fiber.Ctx) error { return nil }
+
+func (app *App) postDSP(c *fiber.Ctx) error { return nil }
